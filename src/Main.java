5cd469e97cca092f8f91ccad15b2993832f1b751
Sosed_11 <kost1991ya@mail.ru>
2022-10-13 20:37:11 +0500

    Курсовая - решение без методов

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.Arrays;
+
 public class Main {
+
+
     public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] % 2 == 0) {
+                array[index] += 1;
+            }
+        }
+        System.out.println(Arrays.toString(array));
+        for (int i = 2; i < array.length - 3; i++) {
 
+            System.out.print(array[i] + " ");
 
+        }
     }
+
 }

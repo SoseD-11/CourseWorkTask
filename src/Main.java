6f783825cfa5db5e8340b312a9a0d784ad0513c9
Sosed_11 <kost1991ya@mail.ru>
2022-10-16 19:32:48 +0500

    Курсовая - задача из билета

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,18 @@
-import java.util.Arrays;
-
 public class Main {
-
-
-    public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    public static void customList(int[]array){
         for (int index = 0; index < array.length; index++) {
             if (array[index] % 2 == 0) {
                 array[index] += 1;
             }
         }
-        System.out.println(Arrays.toString(array));
-        for (int i = 2; i < array.length - 3; i++) {
-
+        for (int i = 2; i < 6; i++) {
             System.out.print(array[i] + " ");
-
         }
     }
+    public static void main(String[] args) {
+        int[] array = {22,33,11,1,4,5,9,10,4,3,2};
+        customList(array);
 
+
+    }
 }
